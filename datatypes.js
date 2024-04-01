486d67875a82a192d880a53cbad0a3c0cf1a9729
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-04-02 03:10:00 +0530

    Update datatypes.js
    learning different types of data types

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // we are using newer version of javascript
+// alert(3+3) we are using nodejs not browser so the compiler shows an error instead of undefined
+let name="madhav" // string
+let age=22 // integer
+let isloggedin="true" // boolean
+
+// integer
+// string
+// char
+// boolean
+// undefined (you haven't assigned any value to the variable declared earlier)
+// null standalone value
+// symbol mostly used in react for uniqueness when using different components
+// objects
+console.log(typeof undefined)
